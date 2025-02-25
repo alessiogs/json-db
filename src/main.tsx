1be045d357c123a8fc9ed024b94d18c41973a08b
alessiogs <alessio.soggiu@hotmail.it>
2025-02-25 15:03:45 +0100

    base path updated for CD

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/json-db" element={<App />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
