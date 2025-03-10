e615afc114d07ffbeca523a3f286a5771f6cb5e0
alessiogs <alessio.soggiu@hotmail.it>
2025-03-10 21:56:57 +0100

    feat(): import data collection from file implemented

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,18 @@
 import { useAtom } from "jotai"
 import { useSearchParams } from "react-router"
 import { collectionsAtom } from "../atoms/jsonAtom"
+import { Button } from "./ui/button"
+import { Plus } from "lucide-react"
+import { Input } from "./ui/input"
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
+import { useState } from "react"
 
 const Sidebar = () => {
   const [searchParams, setSearchParams] = useSearchParams()
@@ -10,7 +22,24 @@ const Sidebar = () => {
       return prev
     })
   }
-  const [collections] = useAtom(collectionsAtom)
+  const [file, setFile] = useState<File | null>(null)
+  const [collections, setCollections] = useAtom(collectionsAtom)
+  const [open, setOpen] = useState(false)
+
+  const handleAddCollection = () => {
+    const collection = file
+    if (collection) {
+      const reader = new FileReader()
+      reader.onload = () => {
+        const data = JSON.parse(reader.result as string)
+        setCollections((prev) => [...prev, { name: collection.name, data }])
+      }
+      reader.readAsText(collection)
+    }
+
+    setFile(null)
+    setOpen(false)
+  }
 
   return (
     <aside className="w-64 bg-gray-900 text-white p-4">
@@ -28,6 +57,28 @@ const Sidebar = () => {
             {collection.name}
           </li>
         ))}
+        <li>
+          <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+              <Button variant="secondary">
+                <Plus />
+                New Collection
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>New Collection</DialogTitle>
+              </DialogHeader>
+              <Input
+                type="file"
+                onChange={(e) => setFile(e.target.files![0])}
+              />
+              <DialogFooter>
+                <Button onClick={handleAddCollection}>Save</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </li>
         {collections.length === 0 && (
           <p className="text-gray-400">No collections found.</p>
         )}
